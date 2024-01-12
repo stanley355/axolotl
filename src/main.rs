e0f2995695df5d8b9b5a476f2bdc64ce63c0768b
Stanley Winata <winatastanley355@gmail.com>
2024-01-12 17:18:38 +0700

    todos create

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 use axum::{
     routing::{get, post},
-    http::StatusCode,
     Json, Router,
 };
 
+use serde::{Deserialize, Serialize};
+
 #[tokio::main]
 async fn main() {
-    // initialize tracing
 
     // build our application with a route
     let app = Router::new()
-    // `GET /` goes to `root`
-    .route("/", get(root));
+        // `GET /` goes to `root`
+        .route("/", post(todos_create));
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // run our app with hyper
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct Register {
+    email: String,
+    password: String
+}
+
+async fn todos_create(Json(input): Json<Register>) -> Json<Register> {
+
+    Json(input)
 }
