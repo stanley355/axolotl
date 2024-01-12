916d4b949abac70bb74ce4c39013b4871acc7b9d
Stanley Winata <winatastanley355@gmail.com>
2024-01-12 16:49:57 +0700

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+
+    // build our application with a route
+    let app = Router::new()
+    // `GET /` goes to `root`
+    .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
 }
