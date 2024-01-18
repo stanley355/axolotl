a08109810013bc3dc602e0c5d8ad37b9ea285dc6
Stanley Winata <winatastanley355@gmail.com>
2024-01-18 09:37:56 +0700

    App state

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use axum::Router;
 
 mod accounts;
+mod app_state;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-
     let router = Router::new().nest("/accounts", accounts::router::account_router());
 
-    // run our app with hyper
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
