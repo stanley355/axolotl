ad554d00651576ebd866415af7b2ef3c4543fd66
Stanley Winata <winatastanley355@gmail.com>
2024-01-19 10:40:55 +0700

    model

--- src/accounts/model.rs
+++ src/accounts/model.rs
@@ -1,10 +1,32 @@
 use crate::app_state::AppState;
-use axum::{extract::State, routing::post, Json, Router};
+use axum::{extract::State, Json};
+use entity::users;
+use sea_orm::{ActiveModelTrait, ActiveValue, EntityTrait};
 
 use super::req::RegisterPayload;
 
-pub async fn register_user(state: State<AppState>, Json(req_body): Json<RegisterPayload>) -> Json<RegisterPayload> {
-    println!("{:?}", req_body);
+pub async fn register_user(
+    state: State<AppState>,
+    Json(body): Json<RegisterPayload>,
+) -> Json<()> {
+    let new_user = users::ActiveModel {
+      id: ActiveValue::NotSet,
+        fullname: ActiveValue::default(),
+        phone_number: ActiveValue::Set(body.phone_number),
+        email: ActiveValue::Set(body.email),
+        password: ActiveValue::Set(body.password),
+        created_at: ActiveValue::default(),
+        updated_at: ActiveValue::default()
+    };
 
-    Json(req_body)
+
+    let res = users::Entity::insert(new_user).exec(&state.db_conection).await;
+
+    println!("{:?}", res.unwrap());
+    // match res {
+    //    Ok(insert_result) => Json(insert_result),
+    //    Err(_) => Json() 
+    // }
+
+    Json(())
 }
